948b4f50aa35f08542c053f7ee34e9f2b678f0bb
Pranay Shah <greatpranayshah@gmail.com>
2018-02-28 00:33:55 +0530

    Basecamp - slack integration complete for todos endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const config = require('./config.json');
-const http = require('http');
+const https = require('https');
 var session = require('express-session')
 var passport = require('passport')
 , util = require('util')
@@ -13,8 +13,13 @@ const { IncomingWebhook } = require('@slack/client');
 const url = "https://hooks.slack.com/services/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";
 const webhook = new IncomingWebhook(url);
 
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 const app = express();
 app.use(morgan('combined'))
+
+
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -27,18 +32,21 @@ passport.use(new Thirty7SignalsStrategy({
   clientID: config.THIRTY7SIGNALS_CLIENT_ID,
   clientSecret: config.THIRTY7SIGNALS_CLIENT_SECRET,
   callbackURL: "https://3txgz0knfb.execute-api.us-east-1.amazonaws.com/dev/auth/37signals/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {
-
-      // To keep the example simple, the user's 37signals profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the 37signals account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
+}, function(accessToken, refreshToken, profile, done) {
+  const params = {
+    TableName: process.env.USERS_TABLE,
+    Item: {
+      id: '1', accessToken: accessToken, refreshToken: refreshToken
+    }
+  };
+  dynamoDb.put(params, (error) => {
+    if (error) console.error(error)
+      else {
+        done(null, profile);
+      } 
     });
-  }
-  ));
+}
+));
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -56,63 +64,61 @@ app.get('/auth/37signals/callback',
   });
 
 app.post('/add', function (reqRelay, resRelay) {
+  const addToBaseCamp = function(token) {
+    const options = {
+      hostname: '3.basecampapi.com',
+      path: '/3959821/buckets/6735076/todolists/932727049/todos.json',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': 'Bearer '+ token,
+        'User-Agent': 'Slackbot (pranayv.shah@st.niituniversity.in)'
+      }
+    };
+    const req = https.request(options, (res) => {
+      console.log(`STATUS: ${res.statusCode}`);
+      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      if (res.statusCode < 400) resRelay.sendStatus(200);
+      else {
+        resRelay.send("Failed syncing with BaseCamp")
+      }
+    });
 
-  const options = {
-    hostname: '3.basecampapi.com',
-    port: 80,
-    path: '/3959821/buckets/6735076/todolists/932727049',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+    req.on('error', (e) => {
+      console.log(`problem with request: ${e.message}`);
+    });
+    req.write(JSON.stringify({'content':reqRelay.body.text}));
+    req.end();
+  }
+  const getAuthorization = () => resRelay.send('You need to try again after authorizing yourself: https://launchpad.37signals.com/authorization/new?client_id=7fbcda1ca5ec67e91c22cd332896d29cbe8ca5f7&redirect_uri=https%3A%2F%2F3txgz0knfb.execute-api.us-east-1.amazonaws.com%2Fdev%2Fauth%2F37signals%2Fcallback&type=web_server');
+  const params = {
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id: '1',
     }
   };
-  console.log(reqRelay.body);
-  resRelay.send("Done");
-/*const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  resRelay.sendStatus(res.statusCode);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
+  dynamoDb.get(params, (error, result) => {
+    if (error || !result || !result.Item.accessToken) {
+      console.error("Error",error);
+      getAuthorization();
+    }
+    else {
+      addToBaseCamp(result.Item.accessToken);
+    }
   });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
-
-req.on('error', (e) => {
-  console.log(`problem with request: ${e.message}`);
-});
-
-// write dat  a to request body
-req.write(reqRelay.body.text);
-req.end();*/
 });
 
 app.post('/notif', function(reqRelay, resRelay) {
-  console.log(reqRelay);
-  console.log('**',reqRelay.body.kind);
-  const options = {
-    hostname: 'hooks.slack.com',
-    port: 80,
-    path: '/services/T9EDMNGG2/B9EKSQ1GC/ivl9zH9MaWC2x1jIwKykojBp',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-    if (reqRelay.body.kind === "todo_created"){
+  if (reqRelay.body.kind === "todo_created"){
     var msg = 'To do created:'+reqRelay.body.recording.title;
     webhook.send(msg, function(err, res) {
-    if (err) {
+      if (err) {
         console.log('Error:', err);
-    } else {
-       resRelay.send({status: 200})
-
+      } else {
         console.log('Message sent: ', res);
-    }
-   });
+        resRelay.send({status: 200})
+      }
+    });
   } else {
     resRelay.send({status: 200});
   }
